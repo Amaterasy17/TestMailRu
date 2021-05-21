d3438ff95e3604f694384dc617dda2fb76c9c1b7
Amaterasy17 <sheb70017@gmail.com>
2021-05-21 03:48:24 +0300

    fixing app

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,76 +1,20 @@
-import './App.css';
 import { Figure } from '@components/Figure/Figure';
 import React from 'react';
+import { PreviewDiv } from './styles';
 
 const App: React.FC = () => {
 	return (
 		<div className='App'>
-			<div className='div-style'>
+			<PreviewDiv>
 				<Figure
 					src='https://images.pexels.com/photos/1647214/pexels-photo-1647214.jpeg?dl&fit=crop&crop=entropy&w=640&h=1006'
-					arWidth={9}
-					arHeight={16}
-				>
-					Автор:{' '}
-					<a href='https://www.pexels.com/@simonmigaj'>Simon Migaj 9 16</a>
-				</Figure>
-			</div>
-			<div className='div-style2' />
-
-			<div className='div-style'>
-				<Figure
-					src='https://images.pexels.com/photos/1647214/pexels-photo-1647214.jpeg?dl&fit=crop&crop=entropy&w=640&h=1006'
-					//src="hahahhaha"
 					arWidth={3}
 					arHeight={5}
 				>
 					Автор:{' '}
-					<a href='https://www.pexels.com/@simonmigaj'>Simon Migaj 3 5</a>
+					<a href='https://www.pexels.com/@simonmigaj'>Simon Migaj</a>
 				</Figure>
-			</div>
-			<div className='div-style'>
-				<Figure
-					src='https://images.pexels.com/photos/1647214/pexels-photo-1647214.jpeg?dl&fit=crop&crop=entropy&w=640&h=1006'
-					//src="hahahhaha"
-					arWidth={3}
-					arHeight={4}
-				>
-					Автор:{' '}
-					<a href='https://www.pexels.com/@simonmigaj'>Simon Migaj 3 4</a>
-				</Figure>
-			</div>
-			<div className='div-style'>
-				<Figure
-					src='https://images.pexels.com/photos/1647214/pexels-photo-1647214.jpeg?dl&fit=crop&crop=entropy&w=640&h=1006'
-					//src="hahahhaha"
-					arWidth={1}
-					arHeight={2}
-				>
-					Автор:{' '}
-					<a href='https://www.pexels.com/@simonmigaj'>Simon Migaj 1 2</a>
-				</Figure>
-			</div>
-
-			<div className='div-style'>
-				<Figure src='hahahhaha' arWidth={16} arHeight={9} />
-			</div>
-
-			<div className='div-style'>
-				<Figure
-					src='https://images.pexels.com/photos/1647214/pexels-photo-1647214.jpeg?dl&fit=crop&crop=entropy&w=640&h=1006'
-					//src="hahahhaha"
-				/>
-			</div>
-			<div className='div-style'>
-				<Figure
-					src='https://images.pexels.com/photos/1647214/pexels-photo-1647214.jpeg?dl&fit=crop&crop=entropy&w=640&h=1006'
-					//src="hahahhaha"
-					arWidth={1}
-					arHeight={5}
-				>
-					Автор: <a href='https://www.pexels.com/@simonmigaj'>Simon Migaj</a>
-				</Figure>
-			</div>
+			</PreviewDiv>
 		</div>
 	);
 };
